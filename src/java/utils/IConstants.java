f7cf3efbe7110185078568d97c65b159f8ee6ba1
Natsuto213 <Natsuto213@gmail.com>
2025-10-04 16:59:35 +0700

    commit

--- src/java/utils/IConstants.java
+++ src/java/utils/IConstants.java
@@ -3,5 +3,7 @@ package utils;
 public interface IConstants {
 
     public final String HOME = "home.jsp";
+    public final String LOGIN = "login.jsp";
+    public final String REGISTER = "register.jsp";
 
 }
